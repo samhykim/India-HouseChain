45ec3d4b26f90e67d39f4219e7c8c52773a66631
Sam Kim <samhykim@gmail.com>
2018-04-29 17:33:49 -0700

    add webpack

--- truffle.js
+++ truffle.js
@@ -1,4 +1,10 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+   networks: {
+       development: {
+           host: '127.0.0.1',  // Change if developing on a test net
+           port: 7545,         // Change this (defaults: Ganache 7545, TestRPC 8545, truffle 9545) 
+           network_id: '*'     // Match any network id
+       }
+   }
 };
+
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
       filename: 'build.js' // The final file will be created in dist/build.js
    },
    module: {
-      loaders: [{
+      rules: [{
          test: /\.css$/, // To load the css in react
          use: ['style-loader', 'css-loader'],
          include: /src/
